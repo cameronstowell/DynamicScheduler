6ee7aef66bb9d261aa0d3d146ef0ef0a6b3dd26a
cameronstowell <camerons1016@gmail.com>
2019-11-22 13:14:51 -0500

    Initial Commit

---
+++ DynamicScheduler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicScheduler
+//
+//  Created by Cameron Stowell on 11/22/19.
+//  Copyright © 2019 Cameron Stowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
