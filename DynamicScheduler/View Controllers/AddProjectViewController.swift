74d696bda1b82d43614bfe314e6817057ef21573
cameronstowell <camerons1016@gmail.com>
2019-12-08 17:57:03 -0500

    Fixed up AddTaskViewController and added a way to escape the keyboard on AddProjectViewController

--- DynamicScheduler/View Controllers/AddProjectViewController.swift
+++ DynamicScheduler/View Controllers/AddProjectViewController.swift
@@ -28,7 +28,12 @@ class AddProjectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
 
